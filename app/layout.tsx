82d909d3f568213b9a1a2a9fb7ad542fd4549e3b
AlanY1an <an1154811577@gmail.com>
2024-11-30 16:30:31 -0500

    Update the analysis

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from 'react-hot-toast';
 import ThemeSwitch from '@/components/theme-switch';
 import ThemeContextProvider from "@/context/theme-context";
 import Footer from "@/components/footer";
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -33,6 +34,7 @@ export default function RootLayout({
           <ActiveSectionContextProvider>
             <Header />
             {children}
+            <Analytics />
             <Footer />
             <Toaster position="top-right" />
             <ThemeSwitch />
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Skills from "@/components/skills";
 import Experience from "@/components/experience";
 import Contact from "@/components/contact";
 
+
 export default function Home() {
   return <main className="flex flex-col items-center px-4">
     <Intro />
